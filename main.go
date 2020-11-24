d2e7d801473e587f24fac404f01d43bda98dac66
ChenYiWen <1447292956@qq.com>
2020-11-24 11:45:18 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:1447292956@q.com\">1447292956@qq.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func main()  {
