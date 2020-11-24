368cd7c2572b80b847f371ee3a47e5e673f39fea
ChenYiWen <1447292956@qq.com>
2020-11-24 15:00:19 +0800

    自动重载

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, welcome to goblog！</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:1447292956@q.com\">1447292956@qq.com</a>")
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
-func main()  {
+func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
