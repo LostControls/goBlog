592111613d3a0f5e7a202008426f78b79320f7cc
ChenYiWen <1447292956@qq.com>
2020-11-25 13:26:11 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -5,21 +5,36 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, welcome to goblog！</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:1447292956@qq.com\">1447292956@qq.com</a>")
+		fmt.Fprint(w, "<h1>Hello, Welcome To GoBlog</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>" + "<p>如有疑惑，请联系我们</p>")
 	}
 }
 
-func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系" + "<a href=\"mailto:1447292956@qq.com\">1447292956@qq.com</a>")
+}
+
+func main()  {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	// 文章详情 列表 or 创建
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+	})
+
+	http.ListenAndServe(":3000", router)
 }
